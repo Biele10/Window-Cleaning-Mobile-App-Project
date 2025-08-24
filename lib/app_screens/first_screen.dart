81a9cccab9e9795a24d4e1eca6e97fe7b3cf8315
Biele <158857885+Biele10@users.noreply.github.com>
2025-08-24 11:52:10 +0100

    new title + extra text widget

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -5,13 +5,32 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ICH BIN REICH'),
+        title: Text('Sponge'),
         backgroundColor: Colors.grey[900],
         centerTitle: true,
         foregroundColor: Colors.grey[50],
       ),
       backgroundColor: Colors.blueGrey,
-      body: Center(child: Image.asset("images/brain.jpg")),
+      body: Stack(
+        children: [
+          Center(child: Image.asset("images/brain.jpg")),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Welcome, Malcom',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
