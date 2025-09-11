07ac952ebc5055668f818c14bffa71cf585bc5f2
Biele <158857885+Biele10@users.noreply.github.com>
2025-09-11 23:23:48 +0100

    navigation bar items now use indexing to display different pages when a different button is pressed, not lots of change in code but did a fair amount of reading through documentation though lmao. this framework is starting to make just a bit of sense haha

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  int _currentIndex = 1; // index for home page which is default page
+  final List<Widget> _pages = [OrdersWidget(), HomeWidget(), SettingsWidget()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _pages[_currentIndex],
       appBar: AppBar(
         title: Text('Sponge'),
         backgroundColor: Colors.grey[900],
@@ -11,26 +20,13 @@ class FirstScreen extends StatelessWidget {
         foregroundColor: Colors.grey[50],
       ),
       backgroundColor: Colors.blueGrey,
-      body: Stack(
-        children: [
-          Center(child: Image.asset("images/brain.jpg")),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Welcome, Malcom',
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.add_chart_rounded),
@@ -41,5 +37,50 @@ class FirstScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class OrdersWidget extends StatelessWidget {
+  const OrdersWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('This is the Orders page.'));
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Center(child: Image.asset("images/brain.jpg")),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Welcome, Malcom',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SettingsWidget extends StatelessWidget {
+  const SettingsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('This is the Settings page.'));
   }
 }
