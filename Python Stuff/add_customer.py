ed7f5e7a407817995b6ab85047d11208a8ad06f3
Biele <158857885+Biele10@users.noreply.github.com>
2025-09-26 17:12:37 +0100

    fixing server side stuff, want to show python on github just not the environment variables lol

---
+++ Python Stuff/add_customer.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, jsonify
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+import mysql.connector   # imports mysql library in order to connect to database
+                # with its functions
+
+
+env_path = Path("variables.env")
+load_dotenv(dotenv_path=env_path)       # loads in private variables
+
+database_connect = mysql.connector.connect(         # establishes connection to database
+    host=os.getenv("database_host"),
+    user=os.getenv("database_user"),
+    password=os.getenv("database_password"),     # information to specify database to add to
+    database=os.getenv("database")      # host, username, password and database name
+)
+
+cursor = database_connect.cursor()   # allows for Python code to execute SQL statements
+
+app = Flask(__name__)
+
+@app.route('/receive', methods=['POST'])
+def receive():      # this function manages which functions are carried out depending on the request type 
+    data = request.get_json()  # this parses the JSON body
+
+    if data.get('RequestType') == "add_customer":
+
+        add_customer(data)       # executes the add_customer function
+
+    return jsonify({"status": "ok", "message": f"Hello!"})
+
+def add_customer(data):         # function that stores customer data in database
+
+    name = data.get('Name')
+    address = data.get('Address')
+    regularity = data.get('Regularity')
+    email = data.get('Email')
+    phone = data.get('Phone')
+    add_info = data.get('AddInfo')
+
+    values = (name, address, regularity, email, phone, add_info)        # creates tuple which can be passed into SQL statement
+
+    cursor.execute("INSERT INTO Customers (Name, Address, Regularity, Email, Phone, Additional_Information) VALUES (%s, %s, %s, %s, %s, %s)", values)       # adds values to database
+
+    database_connect.commit()   # commits the change to the database
+
+
+
+
+
+
