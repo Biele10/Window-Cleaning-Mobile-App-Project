c2bd758815c3132aa4ef529e6c1788a16c1b027b
Biele <158857885+Biele10@users.noreply.github.com>
2025-09-29 18:56:43 +0100

    added text fields to AddJob form

--- lib/app_screens/add_job.dart
+++ lib/app_screens/add_job.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddJob extends StatefulWidget {
   const AddJob({super.key});
@@ -7,17 +10,60 @@ class AddJob extends StatefulWidget {
 }
 
 class _AddJobState extends State<AddJob> {
+  // able to access user inputs
+  final _customerController = TextEditingController();
+  final _timeController = TextEditingController();
+  final _dateController = TextEditingController();
+  final _priceController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return (Form(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Add Job"),
-          backgroundColor: Colors.grey[900],
-          foregroundColor: Colors.grey[50],
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Add Job"),
+        backgroundColor: Colors.grey[900],
+        foregroundColor: Colors.grey[50],
+      ),
+      body: Form(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                TextFormField(decoration: InputDecoration(hintText: "Time")),
+
+                TextFormField(decoration: InputDecoration(hintText: "Date")),
+              ],
+            ),
+          ),
         ),
       ),
-    ));
+    );
   }
+}
+
+Future<http.Response> createAlbum(
+  // function that converts a job into a json and stores it in the job file
+  String name,
+  String address,
+  String regularity,
+  String emailAddress,
+  String phoneNumber,
+  String addInfo,
+) {
+  return http.post(
+    Uri.parse('http://192.168.7.150:5000/receive'),
+    headers: <String, String>{'Content-Type': 'application/json; charset=UTF-8'},
+    body: jsonEncode(<String, String>{
+      // encodes all the saved data into json format
+      'RequestType': 'add_customer', // tells the server the type of request
+      'Name': name,
+      'Address': address,
+      'Regularity': regularity,
+      'Email': emailAddress,
+      'Phone': phoneNumber,
+      'AddInfo': addInfo,
+    }),
+  );
 }
